3bdeaf33806f33780f7f162ec36fe57e26eeca75
Ada <adayke10@gmail.com>
2024-11-26 08:39:15 -0800

    added pid to driving forward, strafing, and turning once april tag is detected in the april tag code i doubt it works tho

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTestOpMode.java
@@ -3,15 +3,22 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import java.util.EnumMap;
+import java.util.Locale;
 
 @SuppressWarnings("unused")
 @Config
@@ -22,8 +29,13 @@ public class PIDTestOpMode extends OpMode {
         FRONTLEFT, FRONTRIGHT, BACKRIGHT, BACKLEFT
     }
 
+    public enum State {
+        IDLE, DISTANCE, DISPLAY, ROTATION
+    }
+
     public EnumMap<MotorNames, DcMotor> motors;
 
+    State state;
     public double error;
     public double output;
 
@@ -32,44 +44,90 @@ public class PIDTestOpMode extends OpMode {
     public DcMotor bl;
     public DcMotor br;
 
-    public static double target = 50; // distance from sensor (50mm)
+    Orientation angles;
+
+    public double currentAngle;
+    public static double targetDistance = 50; // distance from sensor (50mm)
+    public static double targetAngle = 90; // distance from sensor (50mm)
     private DistanceSensor sensorDistance;
+    private BNO055IMU imu;
     private MiniPID pid;
 
     @Override
     public void init() {
         sensorDistance = hardwareMap.get(DistanceSensor.class, "distance");
+        initImu();
         fl = hardwareMap.get(DcMotor.class, "BACKRIGHT");
         fr = hardwareMap.get(DcMotor.class, "BACKLEFT");
         bl = hardwareMap.get(DcMotor.class, "FRONTRIGHT");
         br = hardwareMap.get(DcMotor.class, "FRONTLEFT");
 
+        state = State.IDLE;
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        pid = new MiniPID(0.0, 0.0, 0.0);
-        pid.setSetpoint(target);
+        pid = new MiniPID(0.001, 0.02, 0.0);
+        pid.setSetpoint(targetDistance);
         pid.setOutputLimits(-.5,.5);
         pid.setOutputRampRate(0.1);
+    }
+
+    private void initImu() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
     }
 
     @Override
     public void loop() {
         double distance = sensorDistance.getDistance(DistanceUnit.MM);
-        output = pid.getOutput(distance, target);
-        fl.setPower(output);
-        fr.setPower(output);
-        bl.setPower(output);
-        br.setPower(output);
-        double error = target - distance;
+        if (gamepad1.a) {
+            state = State.DISTANCE;
+        } else if (gamepad1.x) {
+            state = State.DISPLAY;
+        } else if (gamepad1.b) {
+            state = State.ROTATION;
+        } else if (gamepad1.x) {
+            state = State.IDLE;
+        }
+
+        switch (state) {
+            case DISTANCE:
+                output = pid.getOutput(distance, targetDistance);
+                fl.setPower(output);
+                fr.setPower(output);
+                bl.setPower(output);
+                br.setPower(output);
+                double error = targetDistance - distance;
+                telemetry.addData("error", error);
+                telemetry.addData("distance", distance);
+                break;
+            case DISPLAY:
+                output = pid.getOutput(distance, targetDistance);
+                error = targetDistance - distance;
+                telemetry.addData("error", error);
+                telemetry.addData("distance", distance);
+                break;
+            case ROTATION:
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                output = pid.getOutput(angles.firstAngle, targetAngle);
+                fl.setPower(output);
+                fr.setPower(-output);
+                bl.setPower(output);
+                br.setPower(-output);
+                telemetry.addData("heading", angles.firstAngle);
+                break;
+        }
         telemetry.addData("output", output);
-        telemetry.addData("distance", distance);
-        telemetry.addData("target", target);
-        telemetry.addData("error", error);
-        telemetry.addData("P component", error * MiniPID.P);
-        telemetry.addData("I component", error * MiniPID.I);
-        telemetry.addData("D component", error * MiniPID.D);
+
+        telemetry.addData("P component", MiniPID.P);
+        telemetry.addData("I component", MiniPID.I);
+        telemetry.addData("D component", MiniPID.D);
         telemetry.update();
     }
 
@@ -92,4 +150,14 @@ public class PIDTestOpMode extends OpMode {
         });
         // setMotorPower(0);
     }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
 }
+
+// 0.02, 0, 0.01
